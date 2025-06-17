da4ceef17489213262ab59e68ad2186d38351890
Mohamed El Saeed <melsaeed@sumerge.com>
2025-06-17 13:31:38 +0300

    External Spring Bean

---
+++ src/main/java/sumerge/task/springbeancourserecommender/CourseRecommender.java
@@ -0,0 +1,5 @@
+package sumerge.task.springbeancourserecommender;
+
+public interface CourseRecommender {
+    public String recommend();
+}
---
+++ src/main/java/sumerge/task/springbeancourserecommender/CourseRecommenderImplThirdBean.java
@@ -0,0 +1,8 @@
+package sumerge.task.springbeancourserecommender;
+
+public class CourseRecommenderImplThirdBean implements CourseRecommender {
+    @Override
+    public String recommend() {
+        return "Third Course to Lean mock Data";
+    }
+}
